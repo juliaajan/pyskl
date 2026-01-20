5906bcb3838acc0cdf2c17c883e55593aa0b3889
0janssen <ju.ja@hotmail.de>
2026-01-20 22:42:23 +0100

    remove print statements in denormalize.py

--- pyskl/datasets/pipelines/denormalize.py
+++ pyskl/datasets/pipelines/denormalize.py
@@ -5,20 +5,21 @@ from ..builder import PIPELINES
 class DeNormalizeKeypoints:
     """Convert normalized keypoints to original image shape."""    
 
+    #TODO: ggf alle werte deren Ausgangswert größer 1 ist rausschmeissen/auf 0 setzen (weil sind größer als Bildgröße ist)
     def __call__(self, results):
         h, w = results['img_shape']
         keypoint = results['keypoint']
         
-        max_val = np.max(keypoint[..., :2])
-        print("Max keypoint value BEFORE denormalization:", max_val)
+        #max_val = np.max(keypoint[..., :2])
+        #print("Max keypoint value BEFORE denormalization:", max_val)
 
         #multiply normalized keypoints with image width and heigth
         keypoint = keypoint.copy()
         keypoint[..., 0] *= w  #x
         keypoint[..., 1] *= h  #y
 
-        max_val = np.max(keypoint[..., :2])
-        print("Max keypoint value AFTER denormalization:", max_val)
+        #max_val = np.max(keypoint[..., :2])
+        #print("Max keypoint value AFTER denormalization:", max_val)
         
         
         results['keypoint'] = keypoint
