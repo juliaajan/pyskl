9a36e23f8f9afcb0edcdef7ec3f6fffc18f4b3b6
0janssen <ju.ja@hotmail.de>
2026-07-23 12:04:07 +0200

    prepare joint training with nobody config

--- julia/WLASL300/AblationStudies/2_RGBPose_Conv3d/rgbpose_conv3d.py
+++ julia/WLASL300/AblationStudies/2_RGBPose_Conv3d/rgbpose_conv3d.py
@@ -118,5 +118,5 @@ checkpoint_config = dict(interval=1)
 workflow = [('train', 1)]
 evaluation = dict(interval=1, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5), key_indicator='RGBPose_1:1_top1_acc')
 log_config = dict(interval=20, hooks=[dict(type='TextLoggerHook')])
-work_dir = './work_dirs/julia/RGBPose_Conv3d/rgbpose_conv3d_2' #TODO
-load_from = 'work_dirs/julia/RGBPose_Conv3d/rgbpose_conv3d_2/rgbpose_conv3d_init_2.pth' #TODO
+work_dir = './work_dirs/julia/RGBPose_Conv3d/rgbpose_conv3d_3' #TODO
+load_from = 'work_dirs/julia/RGBPose_Conv3d/rgbpose_conv3d_3/rgbpose_conv3d_init_3.pth' #TODO
